cb95b3c278715409ec208a055739f2f3e15230d1
unlim <xiangchh@puwell.com>
2019-12-05 18:00:27 +0800

    1.save

---
+++ src/python3/base_test/BaseGrammar.py
@@ -0,0 +1,23 @@
+# 用于Linux/Unix系统,用来指定解释器路径
+# !/usr/bin/python3
+
+# 指定源文件的编码格式,python3中默认为UTF-8
+# -*- coding: UTF-8 -*-
+
+# 单行注释
+
+"""
+多行注释0
+多行注释1
+多行注释2
+"""
+
+'''
+多行注释0
+多行注释1
+多行注释2
+'''
+
+import keyword
+
+print(keyword.kwlist)
